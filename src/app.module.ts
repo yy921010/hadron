c4fae3063bf7b2306d2656008cb00d164f18383b
charles <charlesmaxwellyoung@gmail.com>
2020-09-29 00:07:02 +0800

    build(global): 新增日志依赖

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { WinstonModule } from 'nest-winston';
 
 @Module({
-  imports: [],
+  imports: [
+    WinstonModule.forRoot({
+      // options
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
+  app.useLogger(app.get(WINSTON_MODULE_NEST_PROVIDER));
 }
 bootstrap();
