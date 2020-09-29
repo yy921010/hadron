19e6b2df41cdb23cf396fce24e4cd17c255581b9
charles <charlesmaxwellyoung@gmail.com>
2020-09-29 23:45:50 +0800

    refactor(package.json): 去除winston

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { WinstonModule } from 'nest-winston';
 
 @Module({
   imports: [
-    WinstonModule.forRoot({
-      // options
-    }),
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
-  app.useLogger(app.get(WINSTON_MODULE_NEST_PROVIDER));
 }
 bootstrap();
