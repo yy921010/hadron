87dd3948cec24e9591a8e87161f663c068d9ecba
charles <charlesmaxwellyoung@gmail.com>
2020-10-15 01:51:35 +0800

    feat: 编写用户模块

--- src/user/user.schema.ts
+++ src/user/user.schema.ts
@@ -1,6 +1,30 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
 @Schema()
-export class User extends Document {}
+export class User extends Document {
+  @Prop()
+  userName: string;
+  password: string;
+  nickName: string;
+  isAdmin: number;
+  userId: string;
+  updateTime: string;
+  createTime: string;
+  pin: number;
+  isSub: number;
+  accountNonExpired: number;
+  accountNonLocked: number;
+  credentialsNonExpired: number;
+  enable: number;
+  mail: number;
+  mailCode: number;
+  role: string;
+  ageLevel: string;
+  clientLimit:number
+  avatarUrl:string
+  client
+  isOnline
+  isLocked
+}
 
 export const UserSchema = SchemaFactory.createForClass(User);
