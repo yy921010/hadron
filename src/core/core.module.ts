4f4052964fdd71fc9ce1e645a9d8882ed8715b4e
charles <charlesmaxwellyoung@gmail.com>
2020-10-17 02:52:47 +0800

    feat: 用户查询

--- src/core/core.module.ts
+++ src/core/core.module.ts
@@ -2,9 +2,13 @@ import { Module } from '@nestjs/common';
 import { ConfigService } from './services/config.service';
 import { APP_INTERCEPTOR } from '@nestjs/core';
 import { TransformInterceptor } from './interceptors/transform.interceptor';
+import { ExcludeNullInterceptor } from './interceptors/excludeNull.interceptor';
 
 @Module({
-  providers: [ConfigService, { provide: APP_INTERCEPTOR, useClass: TransformInterceptor }],
+  providers: [ConfigService,
+    { provide: APP_INTERCEPTOR, useClass: TransformInterceptor },
+    { provide: APP_INTERCEPTOR, useClass: ExcludeNullInterceptor }
+    ],
   exports: [ConfigService],
 })
 export class CoreModule {}
