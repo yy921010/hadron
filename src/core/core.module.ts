7bed4cabc5dc54d5e304399b6d043bdbb635bcfe
charles <charlesmaxwellyoung@gmail.com>
2020-10-17 00:19:35 +0800

    feat: 新增响应拦截器

--- src/core/core.module.ts
+++ src/core/core.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ConfigService } from './services/config.service';
+import { APP_INTERCEPTOR } from '@nestjs/core';
+import { TransformInterceptor } from './interceptors/transform.interceptor';
 
 @Module({
-  providers: [ConfigService],
+  providers: [ConfigService,
+    {provide:APP_INTERCEPTOR,useClass: TransformInterceptor}
+  ],
   exports: [ConfigService],
 })
 export class CoreModule {}
