1a9ba785d24592f844c90d647657af14ce59eef9
charles <charlesmaxwellyoung@gmail.com>
2020-10-29 23:20:33 +0800

    fix: 去除配置文件，后期进行走配置中心

---
+++ src/config/local-config.ts
@@ -0,0 +1,82 @@
+export const appConfig = {
+  hadron: {
+    version: 'local',
+    port: 7001,
+    mongo: {
+      uri: 'mongodb://localhost/tmk',
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    },
+    jwtConfig: {
+      secret: 'nHnsBrgeUoRiuE5UyK9yDYHED^!DW@RqUyK9yDYHED^!DW@Rq',
+      signOptions: {
+        expiresIn: '1800s',
+      },
+    },
+    auth: {
+      salt:
+        'b2Tf3C70XuYcF15f1wY62N5f2Ta1Dpnoe7j6r8yrrYKEb5F14bn0bZ0AhQ66wPpuBE3OHV11RKKb388Dv5X6567ALr7kfwBvVMN2bczK9BSFbM06hbs7kPqa373JEfEM',
+    },
+    upload: {
+      basePath: '/Users/charles/Desktop/images',
+    },
+    log4js: {
+      appenders: {
+        console: {
+          type: 'console',
+        },
+        access: {
+          type: 'dateFile',
+          filename: '/Users/charles/Desktop/hadron-logs/access/access.log',
+          alwaysIncludePattern: true,
+          pattern: 'yyyyMMdd',
+          daysToKeep: 60,
+          numBackups: 3,
+          category: 'http',
+          keepFileExt: true,
+        },
+        app: {
+          type: 'dateFile',
+          filename: '/Users/charles/Desktop/hadron-logs/app-out/app.log',
+          alwaysIncludePattern: true,
+          pattern: 'yyyyMMdd',
+          daysToKeep: 60,
+          numBackups: 3,
+          keepFileExt: true,
+        },
+        errorFile: {
+          type: 'dateFile',
+          filename: '/Users/charles/Desktop/hadron-logs/error/error.log',
+          alwaysIncludePattern: true,
+          pattern: 'yyyyMMdd',
+          daysToKeep: 60,
+          numBackups: 3,
+          keepFileExt: true,
+        },
+        errors: {
+          type: 'logLevelFilter',
+          level: 'ERROR',
+          appender: 'errorFile',
+        },
+      },
+      categories: {
+        default: {
+          appenders: ['console', 'app', 'errors'],
+          level: 'DEBUG',
+        },
+        info: {
+          appenders: ['console', 'app', 'errors'],
+          level: 'info',
+        },
+        access: {
+          appenders: ['console', 'app', 'errors'],
+          level: 'info',
+        },
+        http: {
+          appenders: ['console', 'access'],
+          level: 'info',
+        },
+      },
+    },
+  },
+};
--- src/core/services/config.service.ts
+++ src/core/services/config.service.ts
@@ -1,44 +1,8 @@
 import { Injectable } from '@nestjs/common';
-import * as fs from 'fs';
-import * as YAML from 'yaml';
+import { appConfig } from '../../config/local-config';
 @Injectable()
 export class ConfigService {
-  private ymlObject: any;
-
-  constructor() {
-    if (!this.ymlObject) {
-      const ymlObjects =
-        ['.env.yml', '.env.dev.yml', '.env.prod.yml'].map(item => {
-          const ymlFile = fs.readFileSync(`./src/${item}`, 'utf8');
-          return YAML.parse(ymlFile);
-        }) || [];
-      this.mergeObjectByProfile(ymlObjects);
-    }
-  }
-
-  mergeObjectByProfile(ymlObjects: Array<any>) {
-    if (ymlObjects.length > 0) {
-      const defaultConfig = ymlObjects[0];
-      const devConfig = ymlObjects[1];
-      const prodConfig = ymlObjects[2];
-      const defaultActiveKey = this.getMergeKey4Profile(defaultConfig);
-
-      if (defaultActiveKey === this.getMergeKey4Profile(devConfig)) {
-        Object.assign(defaultConfig.hadron, devConfig.hadron);
-        this.ymlObject = defaultConfig;
-      }
-      if (defaultActiveKey === this.getMergeKey4Profile(prodConfig)) {
-        Object.assign(defaultConfig.hadron, prodConfig.hadron);
-        this.ymlObject = defaultConfig;
-      }
-    } else {
-    }
-  }
-
-  getMergeKey4Profile(defaultConfig): string {
-    return defaultConfig?.hadron?.profile?.active || defaultConfig?.hadron?.profile;
-  }
-
+  private ymlObject: any = appConfig;
   get(objectKey: string): unknown | string | number | any {
     if (this.ymlObject) {
       const hadronConfig = this.ymlObject.hadron;
